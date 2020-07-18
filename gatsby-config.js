99fa746d16e3fab1dd8bea5bb60c1b37559ccee2
Josue Villanueva <idjevm@gmail.com>
2020-07-18 04:01:02 -0400

    Adding styled components and removing layout.css file

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@idjevm`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
